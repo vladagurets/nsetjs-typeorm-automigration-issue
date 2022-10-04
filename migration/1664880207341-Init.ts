f4c69a34f38d7a48adfae8a44db166b130c35034
Vladyslav Ohirenko <aguretsvlad@gmail.com>
2022-10-04 13:44:56 +0300

    chore

--- migration/1664880207341-Init.ts
+++ migration/1664880207341-Init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Init1664876828483 implements MigrationInterface {
-  name = 'Init1664876828483';
+export class Init1664880207341 implements MigrationInterface {
+  name = 'Init1664880207341';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -11,13 +11,16 @@ export class Init1664876828483 implements MigrationInterface {
       `CREATE TABLE \`group\` (\`id\` int NOT NULL AUTO_INCREMENT, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`table_of_ref\` (\`anotherEntityId\` int NOT NULL, \`groupId\` int NOT NULL, PRIMARY KEY (\`anotherEntityId\`, \`groupId\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`group_to_another_entity\` (\`anotherEntityId\` int NOT NULL, \`groupId\` int NOT NULL, PRIMARY KEY (\`anotherEntityId\`, \`groupId\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
       `CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`groupId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`group_to_another_entity\` (\`anotherEntityId\` int NOT NULL, \`groupId\` int NOT NULL, INDEX \`IDX_e47a8e6c2c4e12f209502140a4\` (\`anotherEntityId\`), INDEX \`IDX_b40313bc9675a1dcdfbeee03a1\` (\`groupId\`), PRIMARY KEY (\`anotherEntityId\`, \`groupId\`)) ENGINE=InnoDB`,
+      `CREATE INDEX \`IDX_e47a8e6c2c4e12f209502140a4\` ON \`group_to_another_entity\` (\`anotherEntityId\`)`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX \`IDX_b40313bc9675a1dcdfbeee03a1\` ON \`group_to_another_entity\` (\`groupId\`)`,
     );
     await queryRunner.query(
       `ALTER TABLE \`user\` ADD CONSTRAINT \`FK_974590e8d8d4ceb64e30c38e051\` FOREIGN KEY (\`groupId\`) REFERENCES \`group\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -46,9 +49,8 @@ export class Init1664876828483 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX \`IDX_e47a8e6c2c4e12f209502140a4\` ON \`group_to_another_entity\``,
     );
-    await queryRunner.query(`DROP TABLE \`group_to_another_entity\``);
     await queryRunner.query(`DROP TABLE \`user\``);
-    await queryRunner.query(`DROP TABLE \`table_of_ref\``);
+    await queryRunner.query(`DROP TABLE \`group_to_another_entity\``);
     await queryRunner.query(`DROP TABLE \`group\``);
     await queryRunner.query(`DROP TABLE \`another_entity\``);
   }
